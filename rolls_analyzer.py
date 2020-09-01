a8acef4a1a95a6b7ae9b1421555a9089908216a2
Lewis Gross <43077972+lewisgross1296@users.noreply.github.com>
2020-09-01 00:23:21 -0500

    Create basic rolls_analyzer.py

---
+++ rolls_analyzer.py
@@ -0,0 +1,19 @@
+# Module Imports
+# ---------------------------------------------------------------------------- #
+import math                               # Math Functions
+import matplotlib                         # Plotting Supermodule
+import matplotlib.pyplot as plt           # Plotting Utility
+import numpy as np                         # Multidimensional arrays
+# ---------------------------------------------------------------------------- #
+
+
+class rolls_analyzer():
+    # Constructor
+    def __init__( self,
+                   )
+                   self.rolls = {}
+    
+    def prompt_rolls(array rolls_array):
+        
+        message = string("Please enter your roll or roll set")
+        return message
